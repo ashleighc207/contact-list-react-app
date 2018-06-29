0c82dd98f89baaa71cd22e4294735056c8b1a604
Ashleigh <ashleighc207@gmail.com>
2018-06-29 13:04:32 +0000

    Add initial contact list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+class ContactList extends Component {
+  render(){
+    const people = [
+      {name: 'Keira'},  
+      {name: 'Loma'},  
+      {name: 'Zakary'}  
+    ]
+    
+    return <ul>
+      {people.map(person => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ul>
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-      </div>
+      <ContactList/>
     );
   }
 }
