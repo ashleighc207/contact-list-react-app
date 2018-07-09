da3252f0e0f51bc45322cae3740f08c1fd1bdfd2
Ashleigh <ashleighc207@gmail.com>
2018-07-09 13:22:26 +0000

    Add ListContacts component

---
+++ src/ListContacts.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class ListContacts extends Component {
+    render(){
+        return (
+            <ul className='contact-list'>
+                
+            </ul>
+        )
+    }   
+}
+
+export default ListContacts
