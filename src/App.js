2b7c5baf2e7f46eb68d4f2c8593c6e7c1ac5c8ba
Ashleigh <ashleighc207@gmail.com>
2018-07-09 13:00:14 +0000

    Add initial static contacts array

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import React, { Component } from 'react';
 
+const contacts = [
+  {
+    "id": "keira",
+    "name": "Keira Lind",
+    "email": "keira@reacttraining.com",
+    "avatarURL": ""
+  },
+  {
+    "id": "loma",
+    "name": "Loma Walker",
+    "email": "loma@reacttraining.com",
+    "avatarURL": ""
+  },
+  {
+    "id": "zakary",
+    "name": "Zakary Balistreri",
+    "email": "zakary@reacttraining.com",
+    "avatarURL": ""
+  }
+]
+
 class App extends Component {
   render() {
       return <div>Hello World</div>
