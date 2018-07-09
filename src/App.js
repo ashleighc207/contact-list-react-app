2c841891974b8a3e3d4db06a31419fa654ec974f
Ashleigh <ashleighc207@gmail.com>
2018-07-09 20:06:02 +0000

    Add state to contacts

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 
-const contacts = [
-  {
-    "id": "keira",
-    "name": "Keira Lind",
-    "email": "keira@reacttraining.com",
-    "avatarURL": "https://s3.amazonaws.com/uifaces/faces/twitter/itstotallyamy/128.jpg"
-  },
-  {
-    "id": "loma",
-    "name": "Loma Walker",
-    "email": "loma@reacttraining.com",
-    "avatarURL": "https://s3.amazonaws.com/uifaces/faces/twitter/aiiaiiaii/128.jpg"
-  },
-  {
-    "id": "zakary",
-    "name": "Zakary Balistreri",
-    "email": "zakary@reacttraining.com",
-    "avatarURL": "https://s3.amazonaws.com/uifaces/faces/twitter/leandrovaranda/128.jpg"
-  }
-]
 
 class App extends Component {
+  state = {
+    contacts : [
+      {
+        "id": "keira",
+        "name": "Keira Lind",
+        "email": "keira@reacttraining.com",
+        "avatarURL": "https://s3.amazonaws.com/uifaces/faces/twitter/itstotallyamy/128.jpg"
+      },
+      {
+        "id": "loma",
+        "name": "Loma Walker",
+        "email": "loma@reacttraining.com",
+        "avatarURL": "https://s3.amazonaws.com/uifaces/faces/twitter/aiiaiiaii/128.jpg"
+      },
+      {
+        "id": "zakary",
+        "name": "Zakary Balistreri",
+        "email": "zakary@reacttraining.com",
+        "avatarURL": "https://s3.amazonaws.com/uifaces/faces/twitter/leandrovaranda/128.jpg"
+      }
+    ]
+  }
+  
   render() {
     return (
       <div>
-        <ListContacts contacts={contacts}/>
+        <ListContacts contacts={this.state.contacts}/>
       </div>
     )
   }
