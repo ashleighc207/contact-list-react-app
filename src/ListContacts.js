578c5bc2201560d6f039b72014e299be36ccc705
Ashleigh <ashleighc207@gmail.com>
2018-07-13 15:01:08 +0000

    Add clear search feature

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -17,29 +17,45 @@ class ListContacts extends Component {
         this.setState({ query: query.trim() })
     }
     
+    clearQuery = () => {
+        this.setState({query: ''})
+    }
+    
     render() {
-        let showingContacts;
+        const {contacts, onDeleteContact} = this.props
+        const { query } = this.state
         
+        let showingContacts;
         if (this.state.query){
             const match = new RegExp(escapeRegExp(this.state.query), 'i')
-            showingContacts = this.props.contacts.filter((contact) => match.test(contact.name))
+            showingContacts = contacts.filter((contact) => match.test(contact.name))
         } else {
-            showingContacts = this.props.contacts;
+            showingContacts = contacts;
         }
         
         showingContacts.sort(sortBy('name'))
         
         return (
+            
             <div className='list-contacts'>
                 <div className='list-contacts-top'>
                     <input
                         className='search-contacts'
                         type='text'
                         placeholder='Search'
-                        value={this.state.query}
+                        value={query}
                         onChange={(event) => this.updateQuery(event.target.value)}
                     />
                 </div>
+                
+                {showingContacts.length !== contacts.length && (
+                    <div className='showing-contacts'>
+                        <span>Showing {showingContacts.length} of {contacts.length} total</span>
+                        <button onClick={this.clearQuery}>Show all</button>
+                    </div>
+                )}
+                
+                
                 <ul className='contact-list'>
                     {showingContacts.map((contact) => (
                         <li key={contact.id} className='contact-list-item'>
@@ -50,7 +66,7 @@ class ListContacts extends Component {
                                 <p>{contact.name}</p>
                                 <p>{contact.email}</p>
                             </div>
-                            <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
+                            <button onClick={() => onDeleteContact(contact)} className='contact-remove'>
                                 Remove
                             </button>
                         </li>
